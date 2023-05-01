a701968c46bfb178c904648e0d1421e000e7ecc9
Jirawat Boonkumnerd <jo06942@gmail.com>
2023-05-01 16:34:39 +0700

    docs: update document page and readme
    - update document page
    - update readme

--- page/src/components/Documentation.svelte
+++ page/src/components/Documentation.svelte
@@ -2,6 +2,7 @@
   import Highlight from "svelte-highlight";
   import typescript from "svelte-highlight/languages/typescript";
   import "svelte-highlight/styles/github.css";
+  import { npmLink } from "../const";
 
   let copied = false;
   let copiedTimeout: NodeJS.Timeout;
@@ -11,23 +12,29 @@
 
 const sessDesc: RTCSessionDescriptionInit = {
   type: "offer",
-  sdp: "v=0\r\no=- 4109260023080860376 2 IN IP4 127.0.0.1\r\ns=-\r\nt=0 0\r\na=extmap-allow-mixed\r\na=msid-semantic: WMS\r\n",
+  sdp: "v=0\r\no=- 4109260023080860376 2 IN IP4 127.0.0.1\r\n",
 };
-const options: spdCompact.Options = {}; // options need to be the same for compact and decompact
 
-// compact the RTCSessionDescriptionInit
+const options: spdCompact.Options = { compress: true };
+
+// compact the 'RTCSessionDescriptionInit'
 const compactedSessDesc = spdCompact.compact(sessDesc, options);
 const decompactedSessDesc = spdCompact.decompact(compactedSPD, options);
 
-// compact only the SDP string
+// compact only the SDP string, will return base64 encoded if compress is enabled.
 const compactedSPD = spdCompact.compactSDP(sessDesc.sdp, options);
-// decompact sdp, need to specify if it's offer or answer on the 2nd parameter
-const decompactedSPD = spdCompact.decompactSDP(compactedSPD, true, options);`;
+// for decompact need to specify if it's offer or answer because it's not include in SDP
+const decompactedSPD = spdCompact.decompactSDP(compactedSPD, true, options);
+
+// compact only the SDP string to bytes
+const compactedSPDBytes = spdCompact.compactSDPBytes(sessDesc.sdp, options);
+// decompact the compacted SDP bytes to decompacted string
+const decompactedSPD = spdCompact.decompactSDPBytes(compactedSPDBytes, true, options);`;
 </script>
 
 <h1 class="text-4xl font-bold">SDP Compact</h1>
 
-<a href="https://www.npmjs.com/package/sdp-compact" class="mt-4"
+<a href={npmLink} class="mt-4"
   ><img
     src="https://badge.fury.io/js/sdp-compact.svg"
     alt="npm version"
@@ -42,16 +49,17 @@ const decompactedSPD = spdCompact.decompactSDP(compactedSPD, true, options);`;
 <h2 class="text-2xl font-bold mt-8 mb-4">Why?</h2>
 
 <p>
-  A WebRTC SDP can remove some attributes to compress/compact and share
-  configuration on both offer and answer sides.
+  A WebRTC SDP can remove some of the attributes to compress/compact and share
+  config on both the offer and answer sides.
 </p>
 
 <h2 class="text-2xl font-bold mt-8 mb-4">Features</h2>
 
 <ul class="list-disc pl-6">
   <li>Shorten WebRTC SDP.</li>
-  <li>Options to fixed parameters for both offer and answer side.</li>
+  <li>Options to fixed parameters for both the offer and answer side.</li>
   <li>Compress with zlib deflate then base64.</li>
+  <li>Bytes based allow choosing any encoding.</li>
 </ul>
 
 <h2 class="text-2xl font-bold mt-8 mb-4">Installation</h2>
@@ -73,6 +81,10 @@ const decompactedSPD = spdCompact.decompactSDP(compactedSPD, true, options);`;
 </p>
 
 <h2 class="text-2xl font-bold mb-4">Examples</h2>
+
+<p class="mb-2">
+*For options reference can see <a href="https://github.com/ntsd/sdp-compact#options" class="underline">GitHub README</a>
+</p>
 
 <Highlight
   class="select-all rounded ring-2"
--- page/src/const.ts
+++ page/src/const.ts
@@ -1,1 +1,2 @@
 export const githubLink = "https://github.com/ntsd/sdp-compact";
+export const npmLink = "https://www.npmjs.com/package/sdp-compact";
